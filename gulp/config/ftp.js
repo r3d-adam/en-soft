d3ab6c904c173e2db476bb89b474433acf8b0cc5
Anton Evdokimovich <r3d-adam@users.noreply.github.com>
2024-09-30 21:14:06 +0300

    fixed all

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-	host: 'evdony.c1.biz', // host/folder
-	user: '2071769',
-	password: 'e0153d269757b18',
+	host: '',
+	user: '',
+	password: '',
 	parallel: 1, //5 количество одновременных потоков
 };
